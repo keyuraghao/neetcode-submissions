ac85be5ca71fc07b1e30d58bdea745d00f183c2a
Keyur Aghao <65186247+keyuraghao@users.noreply.github.com>
2026-08-12 17:21:09 -0400

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # This problem can be solved in 2 different ways 
+        # 1. using the bucket sort Time : O(n)
+        # 2. using the heap Time O(klogn)
+
+        # doing with the bucket sort
+        count = {}
+        freq = [[] for i in range(len(nums)+1)]
+
+        for i in nums:
+            if i in count:
+                count[i] += 1
+            else:
+                count[i] = 1
+        
+        for key,val in count.items():
+            freq[val].append(key)
+        
+        ans = []
+
+        for i in range(len(freq)-1,0,-1):
+            for n in freq[i]:
+                ans.append(n)
+                if len(ans) == k:
+                    return ans
