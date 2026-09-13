70a770f06e5d4bbdaae9261b0cecf2ca483a6988
Keyur Aghao <65186247+keyuraghao@users.noreply.github.com>
2026-09-13 18:21:49 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hash_map = {} #character : count
+        for val in s:
+            if val in hash_map:
+                hash_map[val] += 1
+            else:
+                hash_map[val] = 1
+
+        for val in t:
+            if val not in hash_map:
+                return False
+            else:
+                hash_map[val] -= 1
+                if hash_map[val] == 0:
+                    del hash_map[val]
+        
+        return len(hash_map) == 0
