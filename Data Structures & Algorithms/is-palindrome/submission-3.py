77f3bfdca3fa4f8899121d3e6df3b89ff551981c
Keyur Aghao <65186247+keyuraghao@users.noreply.github.com>
2026-09-14 19:00:25 -0400

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,10 @@
+# this can be solved in 2 different ways
+1. 
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newstr = ""
+        for c in s:
+            if c.isalnum():
+                newstr += c.lower()
+        
+        return newstr == newstr[::-1]
